3a68b736d98e5014fada8a9bdc0cab03187c065f
Denis <denisfilipov.12@gmail.com>
2020-10-12 11:48:06 +0200

    using hbs

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,12 +1,16 @@
 package com.teamtreehouse.courses;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/", (req, res) -> "Sup students!");
+        get("/", (req, res) -> {
+               return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
